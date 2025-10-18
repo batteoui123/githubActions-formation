d2647354c27624f1800d90f73e28ce11129225bd
Lenovo <elbatteou.oussama@etu.uae.ac.ma>
2025-10-18 22:33:47 +0100

    commit pre-merge

--- src/test.js
+++ src/test.js
@@ -1,1 +1,13 @@
 console.log("thsi file is created in the test-branch !")
+
+
+
+
+
+
+
+function merging(src,dest){
+    console.log('the distination is :',dest)
+    console.log('the source is : ',src)
+}
+merging('main','test-branch')
